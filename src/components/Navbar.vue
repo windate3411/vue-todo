314084d8daea4760e9e91431dc7d249719db85fb
Danny <windate3411@gmail.com>
2020-01-29 21:50:36 +0800

    finished side bar with v-list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,8 +18,16 @@
 
     <!-- side bar content -->
     <v-navigation-drawer app v-model="drawer" temporary absolute class="info">
-      <p class="error">test</p>
-      <v-icon>mdi-email</v-icon>
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-icon>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -29,7 +37,12 @@ export default {
   name: 'Navbar',
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+        { icon: 'mdi-account', text: 'Team', route: '/team' },
+      ]
     }
   },
 }
