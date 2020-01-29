3d8af3a427e4c7b25b241acabd226e77c9e1797d
Danny <windate3411@gmail.com>
2020-01-29 21:08:07 +0800

    add Navbar component and set up basic design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
+    <Navbar />
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -8,12 +9,13 @@
 
 <script>
 
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
 
   components: {
-
+    Navbar
   },
 
   data: () => ({
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <v-app-bar flat hide-on-scroll>
+      <!-- app title -->
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Danny</span>
+      </v-toolbar-title>
+      <!-- push the logout btn to the right -->
+      <v-spacer></v-spacer>
+      <!-- sign out btn  -->
+      <v-btn text color="grey">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
