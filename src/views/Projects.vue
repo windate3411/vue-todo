a8cc5a09720ebb5566ae9c59f2073be2b63d6d54
Danny <windate3411@gmail.com>
2020-01-31 14:35:18 +0800

    add expansion panel to project.vue

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="projects">
     <h1 class="subtitle-1 grey--text">Projects</h1>
-    <v-container
-      class="my-5"
-    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolorem saepe sunt voluptas ratione quia architecto voluptatum dicta excepturi, blanditiis earum? Iusto nulla hic dolorem quam, temporibus cupiditate blanditiis animi.</v-container>
+    <v-container class="my-5">
+      <v-expansion-panels accordion>
+        <v-expansion-panel v-for="project in myProjects" :key="project.title">
+          <v-expansion-panel-header>{{project.title}}</v-expansion-panel-header>
+          <v-expansion-panel-content class="grey--text">
+            <div class="font-weight-bold">due by {{project.due}}</div>
+            <div>{{project.content}}</div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-container>
   </div>
 </template>
 
@@ -11,7 +19,20 @@
 
 export default {
   name: 'projects',
-  components: {
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+      ]
+    }
+  },
+  computed: {
+    myProjects() {
+      return this.projects.filter(item => item.person === 'The Net Ninja')
+    }
   }
 }
 </script>
