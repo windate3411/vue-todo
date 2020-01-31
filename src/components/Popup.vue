b1b247625753df110af54255238843993e0aaa5d
Danny <windate3411@gmail.com>
2020-01-31 15:30:06 +0800

    add form in popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="text-center">
-    <v-dialog v-model="dialog" width="500">
+    <v-dialog width="500">
       <template v-slot:activator="{ on }">
         <v-btn class="success" dark v-on="on">Add New Project</v-btn>
       </template>
       <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>Add a New Project</v-card-title>
+        <v-card-title class="headline grey lighten-2">Add a New Project</v-card-title>
+        <v-card-text>
+          <v-form class="px-3 text-center">
+            <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
+            <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
+            <v-btn text class="mx-0 mt-3 success" @click="submit">Add new project</v-btn>
+          </v-form>
+        </v-card-text>
       </v-card>
     </v-dialog>
   </div>
@@ -15,8 +22,14 @@
 export default {
   data() {
     return {
-
+      title: '',
+      content: ''
     }
   },
+  methods: {
+    submit() {
+      console.log(`${this.content}`)
+    }
+  }
 }
 </script>
