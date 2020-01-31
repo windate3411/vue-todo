27c4c7548cbf0ee7b1e4fab8f32b896ec748cf22
Danny <windate3411@gmail.com>
2020-01-31 14:42:01 +0800

    add menu to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,6 +9,20 @@
       </v-toolbar-title>
       <!-- push the logout btn to the right -->
       <v-spacer></v-spacer>
+      <!-- menu button -->
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on" color="grey">
+            <v-icon left>mdi-chevron-down</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <!-- sign out btn  -->
       <v-btn text color="grey">
         <span>Sign Out</span>
