93a85f8d362bf739cd6ffc24c36c3fce730f00f1
Danny <windate3411@gmail.com>
2020-01-31 15:43:18 +0800

    add date pikcer to popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -10,6 +10,18 @@
           <v-form class="px-3 text-center">
             <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
             <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
+            <!-- add datepicker -->
+            <v-menu max-width="290">
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  :value="formattedDate"
+                  label="Due date"
+                  prepend-icon="mdi-calendar-range"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker v-model="due"></v-date-picker>
+            </v-menu>
             <v-btn text class="mx-0 mt-3 success" @click="submit">Add new project</v-btn>
           </v-form>
         </v-card-text>
@@ -19,16 +31,25 @@
 </template>
 
 <script>
+import format from 'date-fns/format'
+import parseISO from 'date-fns/parseISO'
+
 export default {
   data() {
     return {
       title: '',
-      content: ''
+      content: '',
+      due: null
     }
   },
   methods: {
     submit() {
       console.log(`${this.content}`)
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(parseISO(this.due), 'do MMM yyyy') : ""
     }
   }
 }
