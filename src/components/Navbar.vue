6b498e4a03742e6c9166b361f3a70b86b97a8fcb
Danny <windate3411@gmail.com>
2020-01-31 14:18:48 +0800

    add avatar to team cards and drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,7 +17,18 @@
     </v-app-bar>
 
     <!-- side bar content -->
-    <v-navigation-drawer app v-model="drawer" temporary class="info">
+    <v-navigation-drawer app v-model="drawer" temporary class="primary">
+      <!-- add personal avatar -->
+      <v-row class="flex-column" align="center">
+        <v-col cols="6" class="mt-5">
+          <v-avatar size="100" class="grey lighten-2">
+            <img src="/avatar-1.png" />
+          </v-avatar>
+          <div class="p white--text subtitle-2 mt-2 ml-2">Danny Wang</div>
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <!-- list contents -->
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-icon>
