fed299a53c29b0bb3923bf42954115284b274487
Danny <windate3411@gmail.com>
2020-02-03 21:23:13 +0800

    modify the menu and commented signout btn for now

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,7 +24,10 @@
         </template>
         <v-list>
           <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-            <v-list-item-title>{{ link.text }}</v-list-item-title>
+            <v-list-item-title>
+              <v-icon left>{{link.icon}}</v-icon>
+              <span>{{ link.text }}</span>
+            </v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
